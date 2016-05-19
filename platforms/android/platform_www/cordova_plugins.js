24f61db2965cde2365be7e093f07c484fe4fdd3c
CamiloAPR <palaciocamilor@gmail.com>
2016-05-19 04:24:29 -0500

    Barcode funcional - 2do adelanto pasajero rutas - adelanto conductor
    >Barcode funcional!! <Sólo con el apk> :yei:
    >ahora lo de rutas para pasajero está funcional con las lecturas
    correctas.
    >Adelanto en la interfaz de conductor.

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -296,6 +296,14 @@ module.exports = [
         "id": "cordova-plugin-geolocation.PositionError",
         "pluginId": "cordova-plugin-geolocation",
         "runs": true
+    },
+    {
+        "file": "plugins/com.phonegap.plugins.barcodescanner/www/barcodescanner.js",
+        "id": "com.phonegap.plugins.barcodescanner.BarcodeScanner",
+        "pluginId": "com.phonegap.plugins.barcodescanner",
+        "clobbers": [
+            "cordova.plugins.barcodeScanner"
+        ]
     }
 ];
 module.exports.metadata = 
@@ -307,7 +315,8 @@ module.exports.metadata =
     "cordova-plugin-media": "2.2.0",
     "cordova-plugin-media-capture": "1.2.0",
     "cordova-plugin-geolocation": "2.2.0",
-    "cordova-plugin-compat": "1.0.0"
+    "cordova-plugin-compat": "1.0.0",
+    "com.phonegap.plugins.barcodescanner": "1.2.0"
 }
 // BOTTOM OF METADATA
 });
--- platforms/browser/www/cordova_plugins.js
+++ platforms/browser/www/cordova_plugins.js
@@ -1,11 +1,21 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/com.phonegap.plugins.barcodescanner/www/barcodescanner.js",
+        "id": "com.phonegap.plugins.barcodescanner.BarcodeScanner",
+        "pluginId": "com.phonegap.plugins.barcodescanner",
+        "clobbers": [
+            "cordova.plugins.barcodeScanner"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "cordova-plugin-whitelist": "1.2.1",
     "cordova-plugin-geolocation": "2.2.0",
-    "cordova-plugin-compat": "1.0.0"
+    "cordova-plugin-compat": "1.0.0",
+    "com.phonegap.plugins.barcodescanner": "1.2.0"
 }
 // BOTTOM OF METADATA
 });
